5b5e28c7490b4cffd457c00e02f093c2f88d938f
babrovka <babrovka@gmail.com>
2014-06-18 14:45:28 +0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Cards::Application.routes.draw do
   match '/delete_cards' => 'cards#destroy_all', as: :delete_cards
   match '/conjoint' => 'cards#conjoint', as: :conjoint
   match '/select_card/:id' => 'cards#select', as: :select_card
-  root :to => 'variants#index'
+  root :to => 'cards#conjoint'
 end
